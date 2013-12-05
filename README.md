ca361ff0ec39b4d396834c27a38b107a5d7f71c1
Mike Gehard <mike.gehard@gmail.com>
2013-12-05 11:10:27 -0700

    Initial rails app.

---
+++ README.md
@@ -0,0 +1,8 @@
+# students.gschool.it
+
+This site is the one stop shop for gSchool students to get information about the course.
+
+## Development
+
+1. Copy config/database.yml.example to config/database.yml. Configure for your local database settings.
+1. Run `rake db:create:all` to create the databases.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Students::Application.config.secret_key_base = 'f4fff57954cb99e899146122aecabd095c5270e02532feae606c00eae6072c87ba642abd8267bd4b21be2e63a2ad3ebec06ddf8d7ae2073ff18d583abce9802e'
