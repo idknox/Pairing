3f5649a5943131f42da05183eb91cf2a3d98cd75
Mike Gehard <mike.gehard@gmail.com>
2013-12-05 13:56:09 -0700

    Make preparation the home page for now.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Students::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  root 'pages#preparation'
 
   get '/preparation' => 'pages#preparation'
 
