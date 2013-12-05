f5b757568ab875dac19bde5183458384cd28dae7
Mike Gehard <mike.gehard@gmail.com>
2013-12-05 12:42:39 -0700

    Get pre-work page in.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Students::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  get '/preparation' => 'pages#preparation'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
