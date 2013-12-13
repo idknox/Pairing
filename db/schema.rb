00f5dbfa4492954c7bece5b94db8d02b9edc07d6
Mike Gehard <mike.gehard@gmail.com>
2013-12-13 15:57:41 -0700

    Add in the timestamps for a user.

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,12 @@ ActiveRecord::Schema.define(version: 20131213155355) do
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "github_username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "github_username"
   end
 
 end
