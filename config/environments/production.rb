29045c715e26c6c176242d7be298073785e5e441
Jeff Dean <jeff@galvanize.it>
2013-12-26 14:17:13 -0700

    configure sendgrid on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,3 +83,13 @@ Students::Application.configure do
   config.action_mailer.default_url_options = {host: 'students-gschool-production.herokuapp.com'}
   config.action_mailer.asset_host = 'http://students-gschool-production.herokuapp.com'
 end
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}
