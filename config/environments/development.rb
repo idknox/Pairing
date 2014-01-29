aab9249117d87dbc47d0aeb4e758e8270396c33e
Mike Gehard <mike.gehard@gmail.com>
2014-01-28 17:42:41 -0700

    Turn off the debugging from asset pipeline
    This will make the dev server output have less lines.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Students::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.active_record.schema_format = :sql
 
