32c6bda1196201b4213a09aae11681682646d37e
Mike Gehard <mike.gehard@gmail.com>
2014-01-29 10:16:40 -0700

    Better styling of feedback errors.

--- app/controllers/feedback_entries_controller.rb
+++ app/controllers/feedback_entries_controller.rb
@@ -6,7 +6,7 @@ class FeedbackEntriesController < SignInRequiredController
   def new
     render 'new', locals: {
         feedback_entry: FeedbackEntry.new,
-        users_that_can_be_given_feedback: User.all
+        users_that_can_be_given_feedback: users_that_can_be_given_feedback
     }
   end
 
@@ -17,12 +17,25 @@ class FeedbackEntriesController < SignInRequiredController
     use_case = GiveFeedback.new(User.find(recipient_id), user_session.current_user, comment)
 
     use_case.success(->(_) { redirect_to feedback_path, :notice => t('feedback.success') })
-    use_case.failure(->(feedback_entry) { render 'new', locals: {feedback_entry: feedback_entry} })
+    use_case.failure(->(feedback_entry) {
+      flash[:error] = t('feedback.error')
+      render 'new',
+             locals: {
+                 feedback_entry: feedback_entry,
+                 users_that_can_be_given_feedback: users_that_can_be_given_feedback
+             }
+    })
 
     use_case.run!
   end
 
   def show
     render 'show', locals: {feedback_entry: FeedbackEntry.given_to(user_session.current_user).find(params['id'])}
+  end
+
+  private
+
+  def users_that_can_be_given_feedback
+    User.all
   end
 end
