f62e7efbabe8d4286434e6fdc31cf7f7b002a0f6
Mike Gehard <mike.gehard@gmail.com>
2014-01-30 16:48:22 -0700

    Properly handle nil first and last names.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :github_id, case_sensitive: false, allow_nil: true
 
   def full_name
-    first_name + " " + last_name
+    "#{first_name} #{last_name}"
   end
 end
