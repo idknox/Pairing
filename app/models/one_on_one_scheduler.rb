dc59dad0ddf7ee712c197f15575e016a8e6acaaa
Mike Gehard <mike.gehard@gmail.com>
2014-03-21 09:27:59 -0600

    Leave a break at 3pm for one on ones

--- app/models/one_on_one_scheduler.rb
+++ app/models/one_on_one_scheduler.rb
@@ -9,7 +9,7 @@ class OneOnOneScheduler
 
   def generate_schedule
     current_students = students.to_a.dup
-    times = %w(1pm 1:15pm 1:30pm 1:45pm 2pm 2:15pm 2:30pm 2:45pm 3pm 3:15pm 3:30pm 3:45pm 4pm 4:15pm 4:30pm 4:45pm)
+    times = %w(1pm 1:15pm 1:30pm 1:45pm 2pm 2:15pm 2:30pm 2:45pm 3:15pm 3:30pm 3:45pm 4pm 4:15pm 4:30pm 4:45pm)
 
     @appointments = []
     times.each do |time|
