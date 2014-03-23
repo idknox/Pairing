21ca80781c4a741aed6f0d59cd95cdfa54b82020
Mike Gehard <mike.gehard@gmail.com>
2014-03-23 16:19:51 -0600

    Bucket specs better based on viewer of feedback

--- spec/actions/view_feedback_spec.rb
+++ spec/actions/view_feedback_spec.rb
@@ -1,46 +1,52 @@
 require 'spec_helper'
 
 describe ViewFeedback do
-  it 'returns a feedback item marked as viewed for the user' do
-    recipient = create_user
-    provider = create_user
 
-    entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
+  describe 'when an instructor is viewing feedback' do
+    it 'returns a feedback item and does not mark it as viewed' do
+      recipient = create_user
+      provider = create_user
+      instructor = create_instructor_user
 
-    feedback_item = ViewFeedback.new(recipient, entry_to_view.id).run!
+      entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
 
-    expect(feedback_item.viewed?).to eq true
+      feedback_item = ViewFeedback.new(instructor, entry_to_view.id).run!
+
+      expect(feedback_item.viewed?).to eq false
+    end
   end
 
-  it 'returns a feedback item marked as viewed for the user if the user is an instructor' do
-    recipient = create_instructor_user
-    provider = create_user
+  describe 'when the recipient is viewing feedback' do
+    it 'returns a feedback item marked as viewed' do
+      recipient = create_user
+      provider = create_user
 
-    entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
+      entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
 
-    feedback_item = ViewFeedback.new(recipient, entry_to_view.id).run!
+      feedback_item = ViewFeedback.new(recipient, entry_to_view.id).run!
 
-    expect(feedback_item.viewed?).to eq true
+      expect(feedback_item.viewed?).to eq true
+    end
+
+    it 'returns a feedback item marked if the recipient is an instructor' do
+      instructor = create_instructor_user
+      provider = create_user
+
+      entry_to_view = create_feedback_entry(recipient: instructor, provider: provider, viewed: false)
+
+      feedback_item = ViewFeedback.new(instructor, entry_to_view.id).run!
+
+      expect(feedback_item.viewed?).to eq true
+    end
   end
 
-  it 'raises ActiveRecord::RecordNotFound if the feedback item is not associated with the user' do
+
+  it 'raises ActiveRecord::RecordNotFound if the feedback item is not given to the viewer' do
     recipient = create_user
     provider = create_user
 
     entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
 
     expect{ViewFeedback.new(provider, entry_to_view.id).run!}.to raise_error(ActiveRecord::RecordNotFound)
-  end
-
-  it 'returns a feedback item for another user if the passed in user is an instructor and does not mark it as viewed' do
-    recipient = create_user
-    provider = create_user
-    instructor = create_instructor_user
-
-    entry_to_view = create_feedback_entry(recipient: recipient, provider: provider, viewed: false)
-
-    feedback_item = ViewFeedback.new(instructor, entry_to_view.id).run!
-
-    expect(feedback_item.viewed?).to eq false
   end
 end
