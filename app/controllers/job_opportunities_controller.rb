95066997728d85c641095dd1a81ce95f5f8fb58f
Kinsey Durham <kinseyann505@gmail.com>
2014-03-26 15:22:00 -0600

    Students can only view their created job opportunities

--- app/controllers/job_opportunities_controller.rb
+++ app/controllers/job_opportunities_controller.rb
@@ -1,12 +1,13 @@
 class JobOpportunitiesController < ApplicationController
   def index
-    job_opportunities = JobOpportunity.all
+    job_opportunities = JobOpportunity.jobs_for_student(user_session.current_user)
     render :index, locals: {job_opportunities: job_opportunities}
   end
 
   def create
     job_parameters = params.require(:job_opportunity).permit!
     job_opportunity = JobOpportunity.new(job_parameters)
+    job_opportunity.user_id = user_session.current_user.id
     if job_opportunity.save
       flash[:notice] = 'Job Opportunity Successfully Created'
     else
--- spec/models/job_opportunity_spec.rb
+++ spec/models/job_opportunity_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe 'JobOpportunity' do
+  describe '.jobs_for_student' do
+    it 'returns only job opportunities for a specific user' do
+      user = User.new(id: 1)
+      user_job_opportunity = JobOpportunity.create!(company_name: "Pivotal", user_id: 1)
+      JobOpportunity.create!(company_name: "Pivotal", user_id: 2)
 
+      expect(JobOpportunity.jobs_for_student(user)).to eq [user_job_opportunity]
+    end
+  end
 end
