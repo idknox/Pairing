cf08ca1643ca1f0aa942153eb44b5ffe28371fa4
Jeff Taggart <jeff@jetaggart.com>
2014-04-23 12:14:50 -0600

    Switch to rails 4 style validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-
   INSTRUCTOR = 1
 
-  validates_uniqueness_of :email, case_sensitive: false
-  validates_uniqueness_of :github_id, case_sensitive: false, allow_nil: true
-  validates_presence_of :email
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :github_id, uniqueness: { case_sensitive: false, allow_nil: true }
+  validates :email, presence: true
 
   belongs_to :cohort
 
