b712820fe49ad582331e5d07ca03adb2c7f8c7ef
Jeff Taggart <jeff@jetaggart.com>
2014-04-23 12:40:25 -0600

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
 Cohort.destroy_all
 
-Cohort.create! do |cohort|
-  cohort.name = "Cohort 1"
-end
+Cohort.create!(name: "Cohort 1", directions: "<p>This is a direction</p>", google_maps_location: "https://www.google.com/maps/embed?pb=!1m5!3m3!1m2!1s0x876bec26e4137699%3A0xf9d8bd928167d4d5!2s1035+Pearl+St%2C+Boulder%2C+CO+80302!5e0!3m2!1sen!2sus!4v1387232536923")
