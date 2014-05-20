fa0de95664bcb3d5a21da4e73aa1d8330e0b8c04
Jeff Taggart <jeff@jetaggart.com>
2014-05-20 07:15:40 -0600

    clean up find user from github action

--- app/actions/find_user_from_github_info.rb
+++ app/actions/find_user_from_github_info.rb
@@ -1,10 +1,19 @@
-FindUserFromGithubInfo = ->(github_info) do
-  user = nil
-  if github_info['email'].present?
-    user = User.find_by(email: github_info['email'].downcase)
+class FindUserFromGithubInfo
+  def self.call(github_info)
+    find_by_email(github_info) || find_by_github_id(github_info)
   end
-  user ||= if github_info['id'].present?
+
+  private
+
+  def self.find_by_email(github_info)
+    return unless github_info['email'].present?
+
+    User.find_by(email: github_info['email'].downcase)
+  end
+
+  def self.find_by_github_id(github_info)
+    return unless github_info['id'].present?
+
     User.find_by(github_id: github_info['id'])
   end
-  user
 end
