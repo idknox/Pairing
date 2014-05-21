6f40410db26fab55def492b5d9f8a0255a37e926
Jeff Taggart <jeff@jetaggart.com>
2014-05-21 11:17:57 -0600

    Tweaked nav bar to work on mobile
    [#71588854]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,16 @@
 //= require underscore
 //= require clicker/app
 //= require_tree .
+$(document).ready(function() {
+  var menu = $('#navigation-menu');
+  var menuToggle = $('#js-mobile-menu');
+
+  $(menuToggle).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle(function(){
+      if(menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  });
+});
