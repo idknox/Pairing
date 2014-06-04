b91ecb28a4f4a43f51ab42e406d00ad20af430dc
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-04 16:02:41 -0600

    forgot to commit Question spec

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe Question do
+  describe ".for_cohort" do
+    it "returns questions for the given cohort" do
+      cohort_question_why = Question.create!(text: "why?", cohort_id: 1)
+      cohort_question_how = Question.create!(text: "how?", cohort_id: 1)
+      different_cohort_question = Question.create!(text: "hello there", cohort_id: 2)
+
+      expect(Question.for_cohort(1)).to match_array([cohort_question_why, cohort_question_how])
+    end
+  end
+end
