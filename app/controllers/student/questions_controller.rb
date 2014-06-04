3e57d1990ea5eff7dbaffd2a32b096e7b4dec1cb
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-04 10:57:34 -0600

    questions require text to be present

--- app/controllers/student/questions_controller.rb
+++ app/controllers/student/questions_controller.rb
@@ -1,12 +1,18 @@
 class Student::QuestionsController < SignInRequiredController
   def index
-    @questions_by_day = Question.all.group_by {|q| q.created_at.to_date }
-    @new_question = Question.new
+    @questions_by_day = Question.all.group_by { |q| q.created_at.to_date }
+    @question = Question.new
   end
 
   def create
-    Question.create!(text: params[:question][:text], cohort_id: user_session.current_user.cohort_id)
-    flash[:notice] = "Question added."
-    redirect_to action: :index
+    @question = Question.new(text: params[:question][:text], cohort_id: user_session.current_user.cohort_id)
+
+    if @question.save
+      flash[:notice] = "Question added."
+      redirect_to action: :index
+    else
+      @questions_by_day = Question.all.group_by { |q| q.created_at.to_date }
+      render :index
+    end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+
+  validates :text, presence: true
+
 end
