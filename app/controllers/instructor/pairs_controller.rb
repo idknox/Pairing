36ccd34ed68a4e5967e615ae09b38d1bde1b0554
Jeff Taggart <jeff@jetaggart.com>
2014-06-16 13:16:27 -0600

    Don't include instructors in pairing generator [#73331288]

--- app/controllers/instructor/pairs_controller.rb
+++ app/controllers/instructor/pairs_controller.rb
@@ -2,7 +2,7 @@ class Instructor::PairsController < InstructorRequiredController
 
   def index
     @cohort = Cohort.find(params[:cohort_id])
-    @students = User.where(cohort_id: @cohort)
+    @students = @cohort.students
 
     @pair_generator = PairGenerator.new(@students)
     @pairs = @pair_generator.random_pairs
--- spec/features/pairs/pairs_spec.rb
+++ spec/features/pairs/pairs_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 feature 'PairGenerator' do
   let(:cohort) { create_cohort(name: 'g2') }
   let!(:student1) { create_user(first_name: "First", last_name: "Student", github_id: '123', cohort: cohort) }
-  let(:instructor) { create_user(first_name: "Github", last_name: "User", github_id: '456', role_bit_mask: User::INSTRUCTOR) }
+  let(:instructor) { create_user(first_name: "Github", last_name: "User", github_id: '456', role_bit_mask: User::INSTRUCTOR, cohort: cohort) }
 
-  scenario 'instructors can generate random pair list' do
+  scenario "instructors can generate random pair list that doesn't include instructors" do
     sign_in(instructor)
 
     click_on 'Cohorts'
