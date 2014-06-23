0bb862120c95dd4dd0066845ef6167a81b22d172
Jeff Taggart <jeff@jetaggart.com>
2014-06-23 10:13:48 -0600

    Validate tracker project URL's and show errors

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,8 +2,12 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :exercise
 
-  validates :user, :exercise, :github_repo_name, presence: true
-  validates :github_repo_name, format: { with: /\A[^\/]*\z/, message: "Repo name cannot contain slashes" }
+  validates :user, :exercise, :github_repo_name, :presence => true
+  validates :github_repo_name, format: {:with => /\A[^\/]*\z/,
+                                        :message => "Repo name cannot contain slashes"}
+  validates :tracker_project_url, format: {:with => /\Ahttps?:\/\/www\.pivotaltracker\.com(.*)\z/,
+                                           :message => "This must be a pivotal tracker url",
+                                           :allow_blank => true}
 
   def user_name
     user.full_name
--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -3,13 +3,48 @@ require "rails_helper"
 describe 'Submission' do
   describe "validations" do
     it "requires the github repo name to not have slashes" do
-      submission = Submission.create(github_repo_name: "gSchool/students.gschool.it")
+      submission = Submission.new(github_repo_name: "gSchool/students.gschool.it")
+      submission.valid?
       expect(submission.errors[:github_repo_name]).to be_present
 
       submission.github_repo_name = "students.gschool.it"
       submission.save
 
       expect(submission.errors[:github_repo_name]).to_not be_present
+    end
+
+    it "requires the tracker url to be the full url of pivotal tracker" do
+      submission = Submission.new(tracker_project_url: "something else")
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to be_present
+
+      submission.tracker_project_url = "http://google.com"
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to be_present
+
+      submission.tracker_project_url = "pivotaltracker.com"
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to be_present
+
+      submission.tracker_project_url = "http://www.pivotaltracker.com"
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to_not be_present
+
+      submission.tracker_project_url = "https://www.pivotaltracker.com"
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to_not be_present
+    end
+
+    it "allows a blank pivotal tracker url" do
+      submission = Submission.new(tracker_project_url: "")
+      submission.valid?
+
+      expect(submission.errors[:tracker_project_url]).to_not be_present
     end
   end
 end
