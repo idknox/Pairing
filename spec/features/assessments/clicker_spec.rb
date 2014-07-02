9128e436315271132615a3206e25575f525636df
John Foley <john@hisfoleyness.com>
2014-07-02 11:13:09 -0700

    skip clicker questions if pusher is not configured

--- spec/features/assessments/clicker_spec.rb
+++ spec/features/assessments/clicker_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 feature "Clicker", js: :true, clicker: true do
+  before do
+    skip("Clicker features skipped - set PUSHER_KEY to run") if ENV["PUSHER_KEY"] == "pusher_key"
+  end
 
   scenario "Instructor sees how many students are in the class" do
     in_browser(:instructor) do
